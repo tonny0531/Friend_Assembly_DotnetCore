fffb296da4bcbedc897355a616a2fd890b498cc5
Jeff <tonny0531@hotmail.com>
2021-04-24 00:24:54 +0800

    AddAttr in csproj

---
+++ Core_FriendAssembly/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FriendAssembly;
+
+namespace Core_FriendAssembly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FriendlyClass friendly = new FriendlyClass(1, "MyFriend");
+            Console.WriteLine(friendly.Id + "  " + friendly.Name);
+        }
+    }
+}
